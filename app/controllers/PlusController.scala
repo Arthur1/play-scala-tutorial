fbbd0902116774c13acbdfa149b8753f645964e8
Kazuki ASAKURA <32762324+Arthur1@users.noreply.github.com>
2022-04-18 18:16:18 +0900

    1日目: 演習問題：足し算をするWebアプリケーションのクエリーパラメーターのログ

--- app/controllers/PlusController.scala
+++ app/controllers/PlusController.scala
@@ -6,9 +6,12 @@ import play.api.mvc._
 
 @Singleton
 class PlusController @Inject() (mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+  val logger = play.api.Logger("plus")
+
   def get(a: Option[Int], b: Option[Int]) =
     Action { implicit request: MessagesRequest[AnyContent] =>
       val messages: Messages = request.messages
+      logger.info(s"parameters: a=$a, b=$b")
       Ok {
         a.map { a =>
           b.map { b =>
