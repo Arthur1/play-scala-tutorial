08c978efc65baf3ce757e50e3d9e385263209a7a
Kazuki ASAKURA <32762324+Arthur1@users.noreply.github.com>
2022-04-18 17:30:24 +0900

    1日目: 演習問題：足し算をするWebアプリケーションのテスト

---
+++ test/controllers/PlusControllerSpec.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import org.scalatestplus.play.PlaySpec
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class PlusControllerSpec extends PlaySpec {
+
+  def controller = new PlusController(stubControllerComponents())
+
+  "get" should {
+    "クエリーパラメータがある場合は「3」というレスポンスを返す" in {
+      val name = "namae"
+      val result = controller.get(Some(1), Some(2))(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === s"3")
+    }
+
+    """クエリーパラメータaがない場合は「Please give arguments of a and b.」というレスポンスを返す""" in {
+      val result = controller.get(Some(1), None)(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === """Please give arguments of a and b.""")
+    }
+
+    """クエリーパラメータbがない場合は「Please give arguments of a and b.」というレスポンスを返す""" in {
+      val result = controller.get(None, Some(2))(FakeRequest())
+      assert(status(result) === 200)
+      assert(contentAsString(result) === """Please give arguments of a and b.""")
+    }
+  }
+}
