c9a368d452bed4d7bec77322899c930a811980f5
Kazuki ASAKURA <32762324+Arthur1@users.noreply.github.com>
2022-04-18 18:13:18 +0900

    1日目: HelloControllerのLog追加

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -6,8 +6,11 @@ import play.api.i18n._
 import play.api.mvc._
 
 class HelloController @Inject() (mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+  val logger = play.api.Logger("hello")
+
   def get(name: Option[String]) = Action { implicit request: MessagesRequest[AnyContent] =>
     val messages: Messages = request.messages
+    logger.info(s"name parameter: $name")
     Ok {
       name
         .map(s => Messages("hello", s))
