19c2a007bb0a8ec00262a47f4ce9263ad68f5753
Kazuki ASAKURA <32762324+Arthur1@users.noreply.github.com>
2022-04-18 17:52:52 +0900

    1日目: HelloControllerのメッセージをi18n

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -2,19 +2,16 @@ package controllers
 
 import javax.inject.Inject
 import javax.inject.Singleton
-import play.api.mvc.AbstractController
-import play.api.mvc.Action
-import play.api.mvc.AnyContent
-import play.api.mvc.ControllerComponents
-import play.api.mvc.Request
+import play.api.i18n._
+import play.api.mvc._
 
-class HelloController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
-  def get(name: Option[String]) =
-    Action { implicit request: Request[AnyContent] =>
-      Ok {
-        name
-          .map(s => s"Hello, $s!")
-          .getOrElse("""Please give a name as a query parameter named "name".""")
-      }
+class HelloController @Inject() (mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+  def get(name: Option[String]) = Action { implicit request: MessagesRequest[AnyContent] =>
+    val messages: Messages = request.messages
+    Ok {
+      name
+        .map(s => Messages("hello", s))
+        .getOrElse(Messages("noQuery"))
     }
+  }
 }
