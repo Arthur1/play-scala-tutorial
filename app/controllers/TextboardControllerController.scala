97195cd6d4294ec9277a4031bc213e701f690546
Kazuki ASAKURA <32762324+Arthur1@users.noreply.github.com>
2022-04-20 11:14:49 +0900

    2日目: 掲示板を作る

---
+++ app/controllers/TextboardControllerController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import java.time.OffsetDateTime
+import javax.inject._
+import play.api.i18n._
+import play.api.mvc._
+import play.api.data.Form
+import play.api.data.Forms._
+
+case class PostRequest(body: String)
+
+@Singleton
+class TextboardController @Inject() (mcc: MessagesControllerComponents) extends MessagesAbstractController(mcc) {
+  private val form = Form(
+    mapping(
+      "post" -> text(minLength = 1, maxLength = 10)
+    )(PostRequest.apply)(PostRequest.unapply)
+  )
+
+  def get = Action { implicit request: MessagesRequest[AnyContent] =>
+    val messages: Messages = request.messages
+    Ok(views.html.textboard.index(PostRepository.findAll, form))
+  }
+
+  def post = Action { implicit request: MessagesRequest[AnyContent] =>
+    form
+      .bindFromRequest()
+      .fold(
+        error => BadRequest(views.html.textboard.index(PostRepository.findAll, error)),
+        postRequest => {
+          val post = Post(postRequest.body, OffsetDateTime.now)
+          PostRepository.add(post)
+          Redirect("/textboard")
+        }
+      )
+  }
+}
